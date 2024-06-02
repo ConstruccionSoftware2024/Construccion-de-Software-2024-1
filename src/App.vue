ea614a81e96818f92d76acfc3f57f320bf487c9c
lucazo <heldersac01@gmail.com>
2024-06-01 21:18:08 -0400

    Creación de interfaz básica de Login y Registro
    Falta agregar estilos. Tiempo 1h 30m

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,46 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>
+    <h1>{{ isLoginPage ? 'Iniciar sesión' : 'Registro' }}</h1>
+    <FormularioLogin v-if="isLoginPage" @login="handleLogin" />
+    <FormularioRegistro v-else @register="handleRegister" />
+    <p>
+      {{ isLoginPage ? '¿No tienes una cuenta?' : '¿Ya tienes una cuenta?' }}
+      <a href="#" @click.prevent="togglePage">{{ isLoginPage ? 'Regístrate' : 'Inicia sesión' }}</a>
+    </p>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script>
+import { ref } from 'vue'
+import FormularioLogin from './components/FormularioLogin.vue'
+import FormularioRegistro from './components/FormularioRegistro.vue'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+export default {
+  components: {
+    FormularioLogin,
+    FormularioRegistro
+  },
+  setup() {
+    const isLoginPage = ref(true)
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+    const handleLogin = (username, password) => {
+      console.log('Iniciando sesión con:', username, password)
+    }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
+    const handleRegister = (username, email, password) => {
+      console.log('Registrando usuario con:', username, email, password)
+    }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
+    const togglePage = () => {
+      isLoginPage.value = !isLoginPage.value
+    }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+    return {
+      isLoginPage,
+      handleLogin,
+      handleRegister,
+      togglePage
+    }
   }
 }
-</style>
+</script>
