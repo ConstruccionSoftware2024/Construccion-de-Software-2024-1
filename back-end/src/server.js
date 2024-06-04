0f685f414c4747005f2579300b0a3702af02550f
angelnbm <abravo21@alumnos.utalca.cl>
2024-06-03 22:06:17 -0400

    intento de verificacion de correo

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -4,6 +4,8 @@ import { MongoClient, ObjectId } from 'mongodb'
 import http from 'http'
 import dotenv from 'dotenv'
 
+import nodemailer from 'nodemailer'
+
 dotenv.config()
 
 // Configuración de la aplicación
@@ -47,31 +49,85 @@ app.get('/', async (req, res) => {
 })
 
 app.post('/register', async (req, res) => {
-  try{
-    const database = client.db('construccion');
-    const collection = database.collection('users');
-    await collection.insertOne(req.body);
+  try {
+    const database = client.db('construccion')
+    const collection = database.collection('users')
+    await collection.insertOne(req.body)
     res.send({ success: true, message: 'Registro exitoso' })
-  }
-  catch(error){
+  } catch (error) {
     console.log(error)
   }
-  
-});
+})
 
 app.post('/checkEmail', async (req, res) => {
   try {
-    const database = client.db('construccion');
-    const User = database.collection('users');
-    const user = await User.findOne({ email: req.body.email });
+    const database = client.db('construccion')
+    const User = database.collection('users')
+    const user = await User.findOne({ email: req.body.email })
 
     if (user) {
-      res.json({ exists: true });
+      res.json({ exists: true })
     } else {
-      res.json({ exists: false });
+      res.json({ exists: false })
     }
+    //-----------------------------------------
+
+    //const nodemailer = require('nodemailer')
+    const e_mail = req.body
+    const mail = {
+      user: 'pruebas.construccion2024@gmail.com',
+      pass: 'Construccion'
+    }
+
+    let transporter = nodemailer.createTransport({
+      host: 'mail.mhdeploy.com',
+      port: 2525,
+      tls: {
+        rejectUnauthorized: false
+      },
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: mail.user, // generated ethereal user
+        pass: mail.pass // generated ethereal password
+      }
+    })
+
+    const sendEmail = async (email, subject, html) => {
+      try {
+        await transporter.sendMail({
+          from: `Equipo <${mail.user}>`, // sender address
+          to: email, // list of receivers
+          subject, // Subject line
+          text: 'Verificacion de correo', // plain text body
+          html // html body
+        })
+      } catch (error) {
+        console.log('Algo no va bien con el email', error)
+      }
+    }
+
+    const getTemplate = (username) => {
+      return `
+            <head>
+                
+            </head>
+            
+            <div id="email___content">
+                <img src="https://http.cat/images/102.jpg" alt="">
+                <h2>Hola ${username}</h2>
+                <p>Para confirmar tu cuenta, ingresa al siguiente enlace</p>
+                <a
+                    href="http://localhost:4000/api/user/confirm/"
+                    target="_blank"
+                >Confirmar Cuenta</a>
+            </div>
+          `
+    }
+    const template = getTemplate(user)
+    sendEmail(e_mail, 'Verificación', template)
+    //-----------------------------------------
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Error del servidor' });
+    console.error(error)
+    res.status(500).json({ error: 'Error del servidor' })
   }
-});
+})
