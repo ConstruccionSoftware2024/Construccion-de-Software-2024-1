b3a75324bb5c80d33c644ba340238601adfa7efc
Constru Software <hvasquez21@alumnos.utalca.cl>
2024-06-07 22:51:23 -0400

    Algoritmo para identificar peligro funcional

--- src/views/VistasGrupoFelipe/HistorialAlumno.vue
+++ src/views/VistasGrupoFelipe/HistorialAlumno.vue
@@ -1,30 +1,41 @@
 <template>
-    <div class="historial">
-      <h1>Nombre Alumno</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Sitio Web</th>
-            <th>Hora de Acceso</th>
-            <th>Link</th>
-            <th>Riesgo</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="sitio in sitios" :key="sitio.nombre">
-            <td>{{ sitio.nombre }}</td>
-            <td>{{ sitio.horaAcceso }}</td>
-            <td>
-              <a :href="sitio.link" target="_blank" class="sitio-link">{{ sitio.link }}</a>
-            </td>
-            <td>{{ sitio.riesgo }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </template>
+  <div class="historial">
+    <h1>Nombre Alumno</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Sitio Web</th>
+          <th>Hora de Acceso</th>
+          <th>Link</th>
+          <th>Riesgo</th>
+          <th>Cambiar</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="sitio in sitios" :key="sitio.nombre">
+          <td>{{ sitio.nombre }}</td>
+          <td>{{ sitio.horaAcceso }}</td>
+          <td>
+            <a :href="sitio.link" target="_blank" class="sitio-link">{{ sitio.link }}</a>
+          </td>
+          <td>{{ sitio.riesgo }}</td>
+          <td>
+            <button v-if="sitio.riesgo !== 'Peligroso'" @click="cambiarRiesgo(sitio, 'Peligroso')">Rojo</button>
+            <button v-if="sitio.riesgo !== 'Dudoso'" @click="cambiarRiesgo(sitio, 'Dudoso')">Amarillo</button>
+            <button v-if="sitio.riesgo !== 'Sin riesgo'" @click="cambiarRiesgo(sitio, 'Sin riesgo')">Verde</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
 
 <script>
+let rojo = ["https://discord.com", "https://www.teamspeak.com", "https://www.skype.com", "Zoom.exe", "Telegram.exe", "WhatsApp.exe", "Snapchat.exe", "TikTok.exe", "YouTube.exe", "Twitch.exe", "Tinder.exe", "Grindr.exe", "https://www.epicidad.com", "https://www.instagram.com"];
+let amarillo = ["Riot.exe", "EpicGames", "Txt", "https://docs.google.com", "Excel", "PowerPoint", "Paint", "Illustrator", "Photoshop", "Premiere", "Acrobat", "Ink"];
+let verde = ["https://pseint.sourceforge.net", "https://www.spyder-ide.org", "Eclipse", "NetBeans", "https://www.jetbrains.com", "PyCharm", "VisualStudio", "CodeBlocks", "DevC++", "SublimeText", "Atom", "Notepad++", "Vim", "Emacs", "Nano", "Gedit", "pgAdmin", "GitBash", "https://replit.com"];
+let palabrasPeligrosas = [];
+
 export default {
   data() {
     return {
@@ -33,28 +44,117 @@ export default {
           nombre: 'Google',
           horaAcceso: '10:00 AM',
           link: 'https://www.google.com',
-          riesgo: 'Bajo'
+          riesgo: this.nuevaApp('https://www.google.com')
         },
         {
           nombre: 'PxxxHub',
           horaAcceso: '10:45 AM',
           link: 'https://www.epicidad.com',
-          riesgo: 'Alto'
+          riesgo: this.nuevaApp('https://www.epicidad.com')
         },
         {
           nombre: 'OnlyCars',
           horaAcceso: '10:52 AM',
           link: 'https://onlycars.nelsonsilva.dev/#/',
-          riesgo: 'Bajo'
+          riesgo: this.nuevaApp('https://onlycars.nelsonsilva.dev/#/')
+        },
+        {
+          nombre: 'Word Online',
+          horaAcceso: '11:00 AM',
+          link: 'https://docs.google.com',
+          riesgo: this.nuevaApp('https://docs.google.com')
+        },
+        {
+          nombre: 'Instagram',
+          horaAcceso: '11:30 AM',
+          link: 'https://www.instagram.com',
+          riesgo: this.nuevaApp('https://www.instagram.com')
+        },
+        {
+          nombre: 'Replit',
+          horaAcceso: '12:00 PM',
+          link: 'https://replit.com',
+          riesgo: this.nuevaApp('https://replit.com')
         }
       ]
     }
+  },
+  methods: {
+    nuevaApp(pagina) {
+      for (let i = 0; i < rojo.length; i++) {
+        if (pagina === rojo[i]) {
+          console.log("La página está en lista roja");
+          return "Peligroso";
+        }
+      }
+      for (let i = 0; i < verde.length; i++) {
+        if (pagina === verde[i]) {
+          console.log("La página está en lista verde");
+          return "Sin riesgo";
+        }
+      }
+      for (let i = 0; i < amarillo.length; i++) {
+        if (pagina === amarillo[i]) {
+          console.log("La página está en lista amarilla");
+          return "Dudoso";
+        }
+      }
+      console.log("Página agregada a lista amarilla por defecto");
+      return "Dudoso";
+    },
+    cambiarRiesgo(sitio, nuevoRiesgo) {
+      // Encuentra el índice del sitio en la lista
+      const index = this.sitios.findIndex(s => s === sitio);
+      if (index !== -1) {
+        // Elimina el sitio de su lista actual
+        const riesgoActual = sitio.riesgo;
+        this.eliminarSitioDeLista(sitio, riesgoActual);
+
+        // Asigna el nuevo riesgo al sitio
+        this.sitios[index].riesgo = nuevoRiesgo;
+
+        // Agrega el sitio a la nueva lista
+        this.agregarSitioALista(sitio, nuevoRiesgo);
+      }
+    },
+    eliminarSitioDeLista(sitio, riesgo) {
+      // Implementa la lógica para eliminar el sitio de su lista actual (roja, amarilla o verde)
+      if (riesgo === "Peligroso") {
+        for (let i = 0; i < rojo.length; i++) {
+          if (sitio.link === rojo[i]) {
+            rojo.splice(i, 1);
+          }
+        }
+      } else if (riesgo === "Dudoso") {
+        for (let i = 0; i < amarillo.length; i++) {
+          if (sitio.link === amarillo[i]) {
+            amarillo.splice(i, 1);
+          }
+        }
+      } else if (riesgo === "Sin riesgo") {
+        for (let i = 0; i < verde.length; i++) {
+          if (sitio.link === verde[i]) {
+            verde.splice(i, 1);
+          }
+        }
+      }
+    },
+    agregarSitioALista(sitio, nuevoRiesgo) {
+      // Implementa la lógica para agregar el sitio a su nueva lista (roja, amarilla o verde)
+      if (nuevoRiesgo === "Peligroso") {
+        rojo.push(sitio.link);
+      } else if (nuevoRiesgo === "Dudoso") {
+        amarillo.push(sitio.link);
+      } else if (nuevoRiesgo === "Sin riesgo") {
+        verde.push(sitio.link);
+      }
+    }
   }
 }
+
 </script>
 
 <style scoped>
-
 .sitio-link {
   color: #00aeff;
 }
@@ -64,7 +164,8 @@ table {
   border-collapse: collapse;
 }
 
-th, td {
+th,
+td {
   padding: 15px;
   text-align: left;
   border-bottom: 1px solid #ddd;
@@ -91,7 +192,8 @@ body.dark-mode table {
   color: #fff;
 }
 
-body.dark-mode th, body.dark-mode td {
+body.dark-mode th,
+body.dark-mode td {
   border-bottom: 1px solid #888;
 }
 
