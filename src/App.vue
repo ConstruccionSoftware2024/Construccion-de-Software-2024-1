ea89d785a13fb969c94061bdec131df2f621cccf
MrFalric <110941527+MrFalric@users.noreply.github.com>
2024-06-09 17:51:21 -0400

    8/6/2024 15:00-17:10 estudio de websockets junto con Pedro Retamal 9/6/2024 15:00-17:51 intentar obtener las Urls externas de pagina(no funciona) e internas(funciona) junto con Pedro Retamal

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,47 @@
 <script setup>
-import { computed, watch, onMounted } from 'vue'
-import { useThemeStore } from '../back-end/src/store.js'
-import Navbar from './components/ComponentesGrupoJoaquin/navBar.vue'
-import { RouterView } from 'vue-router'
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+import { useThemeStore } from '../back-end/src/store.js';
+import Navbar from './components/ComponentesGrupoJoaquin/navBar.vue';
+import { RouterView } from 'vue-router';
 
-const themeStore = useThemeStore()
+const themeStore = useThemeStore();
+const router = useRouter();
 
-const themeClass = computed(() => (themeStore.isDarkMode ? 'dark-mode' : 'light-mode'))
-
-watch(themeClass, (newClass) => {
-  document.body.className = newClass
-})
-
-onMounted(() => {
-  const savedTheme = localStorage.getItem('theme')
-  if (savedTheme) {
-    themeStore.isDarkMode = savedTheme === 'dark'
-  }
-  document.body.className = themeClass.value
-})
-
+// Cambiar tema
 const toggleTheme = () => {
-  themeStore.toggleDarkMode()
-  localStorage.setItem('theme', themeStore.isDarkMode ? 'dark' : 'light')
-}
+  themeStore.toggleDarkMode();
+  localStorage.setItem('theme', themeStore.isDarkMode ? 'dark' : 'light');
+};
+
+// Observar cambios en el tema y aplicarlos al cuerpo del documento
+onMounted(() => {
+  const themeClass = () => (themeStore.isDarkMode ? 'dark-mode' : 'light-mode');
+  const applyThemeClass = (newClass) => {
+    document.body.className = newClass;
+  };
+
+  // Aplicar el tema actual al cargar la aplicación
+  applyThemeClass(themeClass());
+
+  // Observar cambios en el tema y aplicarlos
+  themeStore.$watch('isDarkMode', (newVal) => {
+    applyThemeClass(themeClass());
+  });
+});
+
+// Registro de URLs visitadas internas
+router.beforeEach((to, from, next) => {
+  const currentUrl = to.fullPath;
+  console.log('URL interna actual:', currentUrl);
+  next();
+});
+
+// Registro de URLs visitadas externas
+window.addEventListener('beforeunload', function (event) {
+  const currentUrl = window.location.href;
+  console.log('URL externa actual:', currentUrl);
+});
 </script>
 
 <template>
@@ -45,4 +63,3 @@ const toggleTheme = () => {
   min-height: 90vh;
 }
 </style>
-
