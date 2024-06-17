5430ee4271c2b12c5cbffc5d4c95805badc16b19
PedroRetamal <111097062+PedroRetamal@users.noreply.github.com>
2024-06-17 17:32:44 -0400

    Ver Urls activas usando una extensión (Hecha por Pedro Retamal, Felipe Calderón y Ennzo Ortiz)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="app">
-    <Navbar />
-    <div class="main-content">
-      <RouterView />
+    <div id="app">
+        <Navbar />
+        <div class="main-content">
+            <RouterView />
+        </div>
+        <Footer />
     </div>
-    <Footer />
-  </div>
 </template>
 
 <script setup>
@@ -21,53 +21,53 @@ const themeClass = computed(() => (themeStore.isDarkMode ? 'dark-mode' : 'light-
 const router = useRouter();
 
 watch(themeClass, (newClass) => {
-  document.body.className = newClass
+    document.body.className = newClass
 })
 
 // Cambiar tema
 const toggleTheme = () => {
-  themeStore.toggleDarkMode();
-  localStorage.setItem('theme', themeStore.isDarkMode ? 'dark' : 'light');
+    themeStore.toggleDarkMode();
+    localStorage.setItem('theme', themeStore.isDarkMode ? 'dark' : 'light');
 };
 
 onMounted(() => {
-  const savedTheme = localStorage.getItem('theme')
-  if (savedTheme) {
-    themeStore.isDarkMode = savedTheme === 'dark'
-  }
-  document.body.className = themeClass.value
+    const savedTheme = localStorage.getItem('theme')
+    if (savedTheme) {
+        themeStore.isDarkMode = savedTheme === 'dark'
+    }
+    document.body.className = themeClass.value
 })
 
 // Registro de URLs visitadas internas
 router.beforeEach((to, from, next) => {
-  const currentUrl = to.fullPath;
-  console.log('URL interna actual:', currentUrl);
-  next();
+    const currentUrl = to.fullPath;
+    console.log('URL interna actual:', currentUrl);
+    next();
 });
 
 // Registro de URLs visitadas externas
 window.addEventListener('beforeunload', function (event) {
-  const currentUrl = window.location.href;
-  console.log('URL externa actual:', currentUrl);
+    const currentUrl = window.location.href;
+    console.log('URL externa actual:', currentUrl);
 });
 </script>
 
 <style scoped>
 #app {
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
 }
 
 .main-content {
-  flex: 1;
-  min-height: 90vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    flex: 1;
+    min-height: 90vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 
 footer {
-  flex-shrink: 0;
+    flex-shrink: 0;
 }
 </style>
