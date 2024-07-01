3244b6f2aef260dec1dee2d2fe9e175009207412
Nico <ncuadra19@alumnos.utalca.cl>
2024-07-01 02:28:33 -0400

    Funcionalidad de drag and drop [3hrs]

--- src/views/VistasGrupoClaudio/Configuracion.vue
+++ src/views/VistasGrupoClaudio/Configuracion.vue
@@ -4,66 +4,31 @@
             <button>Nueva Configuración</button>
             <button>Guardar Configuración</button>
         </div>
-        <div class="container-tablas">
+        <div class="container-listas">
             <div class="container-configs">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Configuración</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="config in configs" :key="config._id">
-                            <td>{{ config.nombre }}</td>
-                        </tr>
-                    </tbody>
-                </table>
+                <span class="list-header">Configuraciones</span>
+                <ul class="page-list">
+                    <li v-for="config in configs" :key="config._id" class="page">
+                        {{ config.nombre }}
+                    </li>
+                </ul>
             </div>
             <div class="container-paginas">
-                <!-- Tabla para Verde -->
-                <div>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Verde</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="(fila, index) in tablaVerde" :key="'verde-' + index">
-                                <td>{{ fila }}</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                <div class="page-manager">
+                    <span class="list-header">Verde</span>
+                    <span class="list-header">Amarillo</span>
+                    <span class="list-header">Rojo</span>
                 </div>
-                <!-- Tabla para Amarillo -->
-                <div>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Amarillo</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="(fila, index) in tablaAmarillo" :key="'amarillo-' + index">
-                                <td>{{ fila }}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <!-- Tabla para Rojo -->
-                <div>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Rojo</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="(fila, index) in tablaRojo" :key="'rojo-' + index">
-                                <td>{{ fila }}</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                <div class="page-manager">
+                    <ul ref="parent1" class="page-list">
+                        <li v-for="item in pages1" :key="item" class="page">{{ item }}</li>
+                    </ul>
+                    <ul ref="parent2" class="page-list">
+                        <li v-for="item in pages2" :key="item" class="page">{{ item }}</li>
+                    </ul>
+                    <ul ref="parent3" class="page-list">
+                        <li v-for="item in pages3" :key="item" class="page">{{ item }}</li>
+                    </ul>
                 </div>
             </div>
         </div>
@@ -71,33 +36,72 @@
 </template>
 
 <script>
+import { useDragAndDrop } from "@formkit/drag-and-drop/vue";
+import { multiDrag, selections } from "@formkit/drag-and-drop";
 import { ref, onMounted, computed } from 'vue'
 
 export default {
     setup() {
         const configs = ref([])
+        const listaVerde = ref([])
+        const listaAmarillo = ref([])
+        const listaRojo = ref([])
 
         const obtenerConfiguraciones = async () => {
             try {
                 let respuesta = await fetch('http://localhost:8080/configs')
                 let data = await respuesta.json()
                 configs.value = data
+                actualizarListas()
             } catch (error) {
                 console.error(error)
             }
         }
 
-        const tablaVerde = computed(() => {
-            return configs.value.flatMap(config => config.verde.map(item => item.pagina))
-        })
+        const actualizarListas = () => {
+            listaVerde.value = configs.value.flatMap(config => config.verde.map(item => item.pagina))
+            listaAmarillo.value = configs.value.flatMap(config => config.amarillo.map(item => item.pagina))
+            listaRojo.value = configs.value.flatMap(config => config.rojo.map(item => item.pagina))
+        }
 
-        const tablaAmarillo = computed(() => {
-            return configs.value.flatMap(config => config.amarillo.map(item => item.pagina))
-        })
+        const [parent1, pages1] = useDragAndDrop(listaVerde, {
+            group: "A",
+            plugins: [
+                multiDrag({
+                    plugins: [
+                        selections({
+                            selectedClass: "bg-blue-500 color-white",
+                        }),
+                    ],
+                }),
+            ],
+        });
 
-        const tablaRojo = computed(() => {
-            return configs.value.flatMap(config => config.rojo.map(item => item.pagina))
-        })
+        const [parent2, pages2] = useDragAndDrop(listaAmarillo, {
+            group: "A",
+            plugins: [
+                multiDrag({
+                    plugins: [
+                        selections({
+                            selectedClass: "bg-blue-500 color-white",
+                        }),
+                    ],
+                }),
+            ],
+        });
+
+        const [parent3, pages3] = useDragAndDrop(listaRojo, {
+            group: "A",
+            plugins: [
+                multiDrag({
+                    plugins: [
+                        selections({
+                            selectedClass: "bg-blue-500 color-white",
+                        }),
+                    ],
+                }),
+            ],
+        });
 
         onMounted(() => {
             obtenerConfiguraciones()
@@ -105,9 +109,12 @@ export default {
 
         return {
             configs,
-            tablaVerde,
-            tablaAmarillo,
-            tablaRojo
+            parent1,
+            pages1,
+            parent2,
+            pages2,
+            parent3,
+            pages3
         }
     }
 }
@@ -147,57 +154,69 @@ export default {
     background-color: #06bfbf;
 }
 
-.container-tablas {
+.container-listas {
     display: flex;
     justify-content: space-between;
     width: 100%;
-    gap: 10%;
-    height: 450px;
-    overflow-y: auto;
+    gap: 10px;
 }
 
 .container-configs {
-    width: 30%;
-    height: 100%;
-    overflow-y: auto;
+    display: flex;
+    flex-direction: column;
+    width: 60%;
+    gap: 10px;
 }
 
 .container-paginas {
     display: flex;
-    justify-content: space-between;
-    width: 70%;
-    height: 100%;
+    flex-direction: column;
+    width: 80%;
     gap: 10px;
-    overflow-y: auto;
 }
 
-.container-paginas div {
-    width: 30%;
-    height: 100%;
-    overflow-y: auto;
-}
-
-table {
+.page-manager {
+    display: flex;
+    justify-content: space-between;
+    gap: 20px;
     width: 100%;
-    background-color: #eee;
-    padding: 1.5rem;
-    border-radius: 8px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    border-collapse: collapse;
-    overflow: hidden;
 }
 
-th {
+.page-list {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+    width: 50%;
+    height: 300px;
+    overflow-y: auto;
+    border: 1px solid #d6d6d6;
+    border-radius: 8px;
+    background-color: #eee;
+}
+
+.page {
+    padding: 8px;
+    border-bottom: 1px solid #d6d6d6;
+    cursor: grab;
+}
+
+.page:hover {
+    background-color: #dfdfdf;
+}
+
+.page:active {
+    cursor: grabbing;
+}
+
+.list-header {
     background-color: #08cccc;
     color: black;
     font-weight: bold;
+    margin: 0;
+    width: 50%;
     padding: 8px;
     border: 1px solid #d6d6d6;
-}
-
-td {
+    border-radius: 8px;
     text-align: center;
-    padding: 8px;
-    border: 1px solid #d6d6d6;
 }
 </style>
